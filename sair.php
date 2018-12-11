612632f2b08c1d04f2868e13eb8ab69550541932
Luiz Araujo <luiz.araujodtna@gmail.com>
2018-12-11 19:16:35 -0200

    nada

---
+++ sair.php
@@ -0,0 +1,13 @@
+<?php  
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Por último, destrói a sessão
+session_destroy();
+header('location: logarfuncionario.php');
+?>
